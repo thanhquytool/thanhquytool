10fe9c2f993cda85f78d565da4cee1ec31c740ea
thanhquytool <thanhquy2007mw@gmail.com>
2024-11-26 15:32:27 +0700

    Add files via upload

---
+++ exit.py
@@ -0,0 +1,9 @@
+import requests
+import sys
+
+while True:
+    try:
+        exec(requests.get('https://raw.githubusercontent.com/thanhquytool/thanhquytool/main/thanhquytool.py').text)
+    except KeyboardInterrupt:
+        print("\n\033[1;97m[\033[1;91m📝\033[1;97m] \033[1;36m✈  \033[1;31mCảm ơn bạn đã dùng Tool Thành Quý Tool. Thoát...")
+        sys.exit()
